811f319526c6bb8ec452006a55449cc1875fe95b
pmcgie <pmcgie@gmail.com>
2018-06-06 23:14:05 -0500

    Added Trivia Game Intro

--- TriviaGame/assets/javascript/app.js
+++ TriviaGame/assets/javascript/app.js
@@ -1,17 +1,49 @@
 //Create Variables
 var questions
-var timer
+var count = 5;
+var startit;
+var counter;
 
-var content = '<h1>Message Header!</h1>';
+//Click on Question Mark for introduction of Game
+$('#questionmark').click(function(){
+    
+        $("#myAudio")[0].play();
+        $("#categories").empty();
+        $("#categories").removeAttr('id');
 
+        startit = setTimeout(getReady,1000);
+        clearTimeout(startit);
+        counter = setInterval(function(){updateTimer()},1500)
+        
+});
 
-//Initial Trivia Countdown
+//Initial "Get Ready" Message
+function getReady() {
+     $("#questionmark").html("<h2>Get Ready to Play the Game in...!</h2>").fadeOut(2000);
+}
 
-//Testing out fade, but eventually will be fade out and countdown into the game from 5,4,3,2,1, Trivia Time!
-$(window).keypress(function(e){
-    if(e.keyCode == 32){
-        $('body').fadeOut(1000,function() {
-            $('body').replaceWith(content);
+//Countdown
+function updateTimer(){
+    if(count > 0){
+        $("#questionmark").fadeOut('slow', function(){
+        	$("#questionmark").text(count);
+            $("#questionmark").fadeIn();
+            count--;
         });
     }
-});
+    else if(count === 0){
+        $("#questionmark").fadeOut('slow', function(){
+            $("#questionmark").text(count);
+            $("#questionmark").fadeIn();
+            clearInterval(counter);
+            setTimeout(playTime,1000);
+            clearTimeout(setTimeout);
+        });
+    }
+}
+
+//
+function playTime() {
+    // in the element with an id of time-left add an h2 saying About 10 Seconds Left!
+    $('#questionmark').html("<h2>Time to Play Trivia!<h2>")
+}
