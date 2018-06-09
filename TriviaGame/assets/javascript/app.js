17833d8e743f2e2ccb013e36352b2ef1182b48ff
pmcgie <pmcgie@gmail.com>
2018-06-08 23:06:17 -0500

    further progress on trivia game

--- TriviaGame/assets/javascript/app.js
+++ TriviaGame/assets/javascript/app.js
@@ -1,51 +1,153 @@
 //Create Global Variables
 var questions
-var count = 5;
+var count = 6;
 var startit;
 var counter;
+var questionUsed;
 
-//Create Question Objects
+//Create Question Objects'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+var questionBank = [ 
+    {
+        Question: 'How many different sports has the dog "Air Bud" played in (in the movies)?',
+        QuestionResponses = ["3","4","5","6"],
+        Correct: 2
+        }
+    ,
+    {
+        Question: 'What country are the islands of Islay, Mull, and St. Kilda located?',
+        QuestionResponses = ["Ireland","Scotland","Iceland","Fiji"],
+        Correct: 2
+    },
+    {
+        Question: 'What is the capital of Nebraska?',
+        QuestionResponses = ["Lincoln","Des Moine","Bernard","Lars"],
+        Correct: 0
+    },
+    {
+        Question: 'How many NBA Championships did Michael Jordan win?',
+        QuestionResponses = ["4","5","6","7"],
+        Correct: 2
+    },
 
+    {
+        Question: 'What programming language does Excel use?',
+        QuestionResponses = ["VBA","Python","Java","Bombay"],
+        Correct: 0
+    },
 
-//Click on Question Mark for introduction of Game
-$('#questionmark').click(function(){
+    {
+        Question: 'In the 1979 NFL Draft, Joe Montana was selected with what number pick in the draft?',
+        QuestionResponses = ["5","22","40","82"],
+        Correct: 3
+    },
+
+    {
+        Question: 'Which song was not released in the 80s?',
+        QuestionResponses = ["B-52's: Love Shack","Tom Petty: Into the Great Wide Open","Michael Jackson: Billy Jean","Prince: When Doves Cry"],
+        Correct: 1
+    },
+
+    {
+        Question: 'Which one is not a cryptocurrency?',
+        QuestionResponses = ["Bitcoin","Mintcoin","Swiftcoin","Potcoin"],
+        Correct: 1
+    },
+
+    {
+        Question: 'Which character is not a marvel comic books character?',
+        QuestionResponses = ["Frog-Man","Owl","Aqua Man","Iron Man"],
+        Correct: 2
+    },
+
+    {
+        Question: 'When year was the Apple iPhone Introduced?',
+        QuestionResponses = ["2005","2010","2001","2007"],
+        Correct: 3
+    }
+
+]
+
+//End Question Objects''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+//Create Timer Object'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+var timer = {
+
+    startTime: 30,
+
+    //Kicks off timer
+    start: function() {
+        intervalId = setInterval(timer.count,1000);
+        clockRunning = true;
+    },
+
+    //Times up Next Question
+    stop: function() {
+        clearInterval(intervalId);
+        $("#timer").text("Times up");
+    },
+
+    //Decrease time by 1
+    count: function() {
+        $("#timer").html(timer.time--)
+    },
+}
+//End Timer Objects and Methods''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+//Starting the Game''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+$('.questionmark').click(function(){
     
-        $("#myAudio")[0].play();
-        $("#categories").empty();
-        $("#categories").removeAttr('id');
+    //$("#myAudio")[0].play();
+    $("#instruction").empty();
+    $("#instruction").removeAttr('id');
 
-        startit = setTimeout(getReady,1000);
-        clearTimeout(startit);
-        counter = setInterval(function(){InitialTimer()},1500)
-        
+    setTimeout(getReady,1000*2);
+    setTimeout(playTime,1000*13);
+    setTimeout(startQuestions,1000*14);
+    var counter = setTimeout(startInterval,1000);
 });
 
-//Initial "Get Ready" Message
-function getReady() {
-     $("#questionmark").html("<h2>Get Ready to Play the Game in...!</h2>").fadeOut(2000);
-}
 
-//Countdown
-function InitialTimer(){
-    if(count > 0){
-        $("#questionmark").fadeOut('slow', function(){
-        	$("#questionmark").text(count);
-            $("#questionmark").fadeIn();
-            count--;
-        });
+//Hype up message/introduction messages''''''''''''''''''''''''''''''''''''''''''''''''''
+
+    //Function for interval
+    function startInterval() {
+        setInterval(function(){InitialTimer()},1500)
     }
-    else if(count === 0){
-        $("#questionmark").fadeOut('slow', function(){
-            $("#questionmark").text(count);
-            $("#questionmark").fadeIn();
+
+    //Countdown to game start
+    function InitialTimer(){
+        if(count > 0){
+            $(".questionmark").fadeOut('slow', function(){
+                $(".questionmark").text(count);
+                $(".questionmark").fadeIn();
+                count--;
+            });
+        }
+        if(count === 0){
             clearInterval(counter);
-            setTimeout(playTime,1000);
-        });
+        } 
+    }
+
+    function getReady() {
+        $(".questionmark").html("<h2>Get Ready to Play the Game!</h2>").fadeOut(2000);
+    }
+    function playTime() {
+        $('.questionmark').html("<h2 style:'text-align: center;'>It's Time to Play Trivia!<h2>")
+    }
+//end of section'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+//Question Time'''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+function startQuestions(){
+
+    //Remove ridiculous question mark so user can never play ridiculous by clicking it now
+    $('.questionmark').empty();
+    $('.questionmark').removeClass('.questionmark');
+    $("#myAudio")[0].pause();
+    questionUsed = [];
+    
+    //loop through questions
+    for (i=0;i<10;i++) {
+        var tid = setInterval(mycode, 1000);
     }
 }
-
-//
-function playTime() {
-    // in the element with an id of time-left add an h2 saying About 10 Seconds Left!
-    $('#questionmark').html("<h2>Time to Play Trivia!<h2>")
-}
+//Question Timer and generate random question from question banks''''''''''''''''''''''''
