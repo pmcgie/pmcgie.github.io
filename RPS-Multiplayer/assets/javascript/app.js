8e7063b033eaaa1217088084eadf3ba34049de88
pmcgie <pmcgie@gmail.com>
2018-06-26 12:15:05 -0500

    Further updates on RPS Game

--- RPS-Multiplayer/assets/javascript/app.js
+++ RPS-Multiplayer/assets/javascript/app.js
@@ -1,4 +1,4 @@
-// Initialize Firebase
+// Initialize Firebase''''''''''''''''''''''''''''''''''''''
   var config = {
     apiKey: "AIzaSyCsOzDKSbPN2PJarxOu_hlwrdEIJyUVwjY",
     authDomain: "rps-game-feb2f.firebaseapp.com",
@@ -10,11 +10,7 @@
   firebase.initializeApp(config);
 
 
-//reset local storage for game
-localStorage.setItem("playerName","None");
-localStorage.setItem("playerStatus","None");
-
-//Global Variables
+//Global Variables'''''''''''''''''''''''''''''''''''''''''''
   //Array Variables (associated with Firebase)
   var curUserList;
   var passwordList;
@@ -25,12 +21,17 @@ localStorage.setItem("playerStatus","None");
   var P2_Status;
   var storedStatus;
   var user = "unknown";
+  var playerRef;
 
-  //Set
+  //Set database
   var database = firebase.database();
 
+  //reset session  storage
+  sessionStorage.clear();
+  sessionStorage.setItem("player",0)
 
-//Grab User List
+
+//Set up game on load'''''''''''''''''''''''''''''''''''''''
 database.ref().on("value",function(snapshot) {
 
   //Grab User Information
@@ -41,13 +42,266 @@ database.ref().on("value",function(snapshot) {
 
   //Check Game information
   P1_Player = snapshot.val().Game.PlayerOne.Name;
-  P2_Player = snapshot.val().Game.PlayerTwo.Name;
   P1_Status = snapshot.val().Game.PlayerOne.status;
+  P2_Player = snapshot.val().Game.PlayerTwo.Name;
   P2_Status = snapshot.val().Game.PlayerTwo.status;
+
+  //Record Information
+  P1_Wins = snapshot.val().Game.PlayerOne.Wins;
+  P1_Losses = snapshot.val().Game.PlayerOne.Losses;
+  P2_Wins = snapshot.val().Game.PlayerTwo.Wins;
+  P2_Losses = snapshot.val().Game.PlayerTwo.Losses;
+
+  //Game Choice
+  P1_Choice = snapshot.val().Game.PlayerOne.play;
+  P2_Choice = snapshot.val().Game.PlayerTwo.play;
+
+
+  //Populate Current Players information
+  if (P1_Status === "None") {
+    $("#P1_Name").text('Waiting on Player')
+  } else {
+    $("#P1_Name").text(P1_Player);
+    $("#P1_Record").text("Wins: " + P1_Wins + " Losses: " + P1_Losses)
+  }
+
+  if (P2_Status === "None") {
+    $("#P2_Name").text('Waiting on Player')
+  } else {
+    $("#P2_Name").text(P2_Player);
+    $("#P2_Record").text("Wins: " + P2_Wins + " Losses: " + P2_Losses)
+  }
+
+  if (P1_Choice !="None") {
+    $("#P1_Options").hide()
+    $("#P1_Record").hide()
+    $("#P1_Decision").text("player one has submitted response, waiting for player two")
+  }
+
+  if (P2_Choice !="None") {
+    $("#P2_Options").hide()
+    $("#P2_Record").hide()
+    $("#P2_Decision").text("player two has submitted response, waiting for player one")
+  }
+
+  if (P1_Choice !="None" && P2_Choice !="None") {
+    var WinnerOutome = decideWinner(P1_Choice,P2_Choice);
+    
+    if (WinnerOutome === 1) {
+        $("#WinnerReveal").text("Player One Wins!")
+    } else if (WinnerOutome === 2) {
+        $("#WinnerReveal").text("Player Two Wins!")
+    } else {
+      $("#WinnerReveal").text("Tie Game.")
+    }
+    
+  }
 
 })
 
-//Sign up - create new user account
+//'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
+//Decide Winner'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+function decideWinner(P1_Choice,P2_Choice) {
+  if (P1_Choice =="Rock" && P2_Choice =="Scissors") {
+      return 1;
+
+  } else if (P1_Choice == "Scissor" && P2_Choice == "Rock") {
+      return 2;
+
+  } else if (P1_Choice == "Paper" && P2_Choice == "Rock") {
+      return 1;
+
+  } else if (P1_Choice == "Rock" && P2_Choice == "Paper") {
+      return 2;
+
+  } else if (P1_Choice == "Scissors" && P2_Choice == "Paper") {
+      return 1;
+
+  } else if (P1_Choice == "Paper" && P2_Choice == "Scissors") {
+      database.ref("GameWinner").set(2);
+      return 2;
+    
+  } else {
+      return 0;
+  }
+}
+
+//player Submits Choice (updates user choice in Firebase)''''''''''''''''''''''''''''''''''''''
+$("#P1_Rock").on("click",function (event){
+    var check = sessionStorage.getItem('player')
+    if (check == 1) {
+      playerplayRef = playerRef.child('play')
+      playerplayRef.set ("Rock")
+    }
+})
+
+$("#P1_Paper").on("click",function (event){
+  var check = sessionStorage.getItem('player')
+  if (check == 1) {
+    playerplayRef = playerRef.child('play')
+    playerplayRef.set ("Paper")
+    }
+})
+
+$("#P1_Scissors").on("click",function (event){
+  var check = sessionStorage.getItem('player')
+  if (check == 1) {
+    playerplayRef = playerRef.child('play')
+    playerplayRef.set ("Scissors")
+  }
+})
+
+$("#P2_Rock").on("click",function (event){
+  var check = sessionStorage.getItem('player')
+  if (check == 2) {
+    playerplayRef = playerRef.child('play')
+    playerplayRef.set ("Rock")
+  }
+})
+
+$("#P2_Paper").on("click",function (event){
+  var check = sessionStorage.getItem('player')
+  if (check == 2) {
+    playerplayRef = playerRef.child('play')
+    playerplayRef.set ("Paper")
+  }
+})
+
+$("#P2_Scissors").on("click",function (event){
+  var check = sessionStorage.getItem('player')
+  if (check == 2) {
+    playerplayRef = playerRef.child('play')
+    playerplayRef.set ("Scissors")
+  }
+})
+
+
+//User Login'''''''''''''''''''''''''''''''''''''''''''''''
+$("#Login").on("click", function (event) {
+
+  //User Name and Password values
+  var user = $("#username").val();
+  var password = $("#password").val();
+
+  //Clear inputs after submittal
+  $("#username").val('');
+  $("#password").val('');
+
+
+    if (curUserList.includes(user)) {
+
+      //Find Index of user
+      var indexNum = curUserList.findIndex(userID => userID ===user)
+
+      var Wins = curWinsList[indexNum];
+      var Losses = curLostList[indexNum];
+
+      if (curPasswordList[indexNum]===password) {
+
+          //Change Player One Status and other Validations
+          if (P1_Status ==="None") {
+            P1_Status = "In-Play";
+
+            //Set session  storage to identify player
+            sessionStorage.setItem("player", 1);
+
+            playerRef = database.ref("Game/PlayerOne");
+
+            //Change Player One Game Info in Firebase
+            playerRef.set ({
+              Name: user,
+              status: P1_Status,
+              Wins: Wins,
+              Losses: Losses,
+              play: "None"
+            })
+
+            playerRef.onDisconnect().set ({
+              Name: "None",
+              status: "None",
+              Wins: 0,
+              Losses: 0,
+              play: "None"
+            })
+
+
+          //Change Player Two Status and Other Validation
+          } else if (P2_Status ==="None" && sessionStorage.getItem('player')!=1) {
+            P2_Status = "In-Play";
+
+            //Set session  storage to identify player
+            sessionStorage.setItem("player", 2);
+
+            //Change Player Two Game Info in Firebase
+            playerRef = database.ref("Game/PlayerTwo");
+
+            playerRef.set ({
+              Name: user,
+              status: P2_Status,
+              Wins: Wins,
+              Losses: Losses,
+              play: "None"
+            })
+
+            playerRef.onDisconnect().set ({
+              Name: "None",
+              status: "None",
+              Wins: 0,
+              Losses: 0,
+              play: "None"
+            })
+
+          } else {
+            alert("(1)Two Players are already playing this game, you will need to wait until one exits or (2) You are trying to play against yourself which is not allowed.")        
+          }
+      
+      //If password does not match with user this message appears
+      } else {
+        alert("Invalid Account. Try again or create new account.")
+      }
+      
+    //If person is logging and user name does not exist
+    } else {
+      alert("Invalid Account. Try again or create new account.")
+    }
+  }
+
+);
+
+
+//This for the Chat Area''''''''''''''''''''''''''''''''''''''''''''''''''''
+$("#message").keypress(function(e) {
+  if(e.which == 13) {
+    addMessage() 
+  }
+});
+
+//Post Message
+$("#sendMessage").on("click", function (event) {
+  addMessage() 
+});
+
+function addMessage() {
+    
+  //Set up for adding row to table
+    var tableBody = $("tbody");
+    var tRow = $("<tr>");
+    var tdUser = $("<td>").text(user);
+    var tdMessage = $("<td>").text($("#message").val());
+  
+    tRow.append(tdUser, tdMessage);
+    tableBody.append(tRow);
+
+    $("#message").val('');
+}
+
+
+//Other Items not impacting game play, only database side'''''''''''''''''''''''''''''
+
+
+//Sign up - ability to create user account''''''''''''''''''''''
 $("#Signup").on("click",function(event) {
 
   //User Name and Password values
@@ -80,130 +334,6 @@ $("#Signup").on("click",function(event) {
       WinsList: curWinsList,
       LossesList: curLostList
     })
-
-  }
-
-});
-
-//User Login Action'''''''''''''''''''''''''''''''''''''''''''''''
-$("#Login").on("click", function (event) {
-
-  //User Name and Password values
-  var user = $("#username").val();
-  var password = $("#password").val();
-
-  //Clear inputs after submittal
-  $("#username").val('');
-  $("#password").val('');
-
-  var storedName = localStorage.getItem("playerName")
-  storedStatus = localStorage.getItem("playerStatus")
-
-  if (storedStatus ==="In-Play") {
-
-    } else {
-
-
-    if (curUserList.includes(user)) {
-
-      //Find Index of user
-      var indexNum = curUserList.findIndex(userID => userID ===user)
-
-      if (curPasswordList[indexNum]===password) {
-
-          //Change Player One Status and other Validations
-          if (P1_Status ==="None") {
-            P1_Status = "In-Play";
-
-            //Change Player One Game Info in Firebase
-            database.ref("Game/PlayerOne").set ({
-              Name: user,
-              status: P1_Status
-            })
-
-            //Update Player One DOM
-            $("#P1_Name").empty();
-            $("#P1_Record").empty();
-
-            $("#P1_Name").text(user);
-            $("#P1_Record").text("Wins: " + curWinsList[indexNum] + " Losses: " + curLostList[indexNum])
-          
-            //Update Local Storage for P1
-            localStorage.setItem("playerName",user);
-            localStorage.setItem("playerStatus",P1_Status);
-
-
-          //Change Player Two Status and Other Validation
-          } else if (P2_Status ==="None" && user !=P2_Player) {
-            P2_Status = "In-Play";
-            P2_Status = user;
-
-            //Change Player Two Game Info in Firebase
-            database.ref("Game/PlayerTwo").set ({
-              Name: user,
-              status: P2_Status
-            })
-
-            //Update Player Two DOM
-            $("#P2_Name").empty();
-            $("#P1_Record").empty();
-
-            $("#P2_Name").text(user);
-            $("#P2_Record").text("Wins: " + curWinsList[indexNum] + " Losses: " + curLostList[indexNum])
-          
-            //Update Local Storage for P2
-            localStorage.setItem("playerName",user);
-            localStorage.setItem("playerStatus",P2_Status);
-
-
-          } else if(user ===  P2_Player) {
-            alert("User can not play itself. Need another user to join.")
-
-          } else {
-            alert("Two Players are already playing this game, you will need to wait until one exits.")        
-          }
-
-      
-      //If password does not match with user this message appears
-      } else {
-        alert("Invalid Account. Try again or create new account.")
-      }
-      
-    //If person is logging and user name does not exist
-    } else {
-      alert("Invalid Account. Try again or create new account.")
-    }
-  }
-
-});
-
-//Need command that when user leaves browser it will change local storage status and kick them out of game
-// Execute a function when the user releases a key on the keyboard
-$("#message").keypress(function(e) {
-  if(e.which == 13) {
-    addMessage() 
   }
 });
-
-//Post Message
-$("#sendMessage").on("click", function (event) {
-  addMessage() 
-});
-
-function addMessage() {
-    
-  //Set up for adding row to table
-    var tableBody = $("tbody");
-    var tRow = $("<tr>");
-    var tdUser = $("<td>").text(user);
-    var tdMessage = $("<td>").text($("#message").val());
-  
-    tRow.append(tdUser, tdMessage);
-    tableBody.append(tRow);
-
-    $("#message").val('');
-}
-
-
-
-
+//'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
