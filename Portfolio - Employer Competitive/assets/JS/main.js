637a439faafbf3893582a69cdaacce3122d5870f
pmcgie <pmcgie@gmail.com>
2018-07-09 17:48:28 -0500

    updated portfolio, still ongoing process

--- Portfolio - Employer Competitive/assets/JS/main.js
+++ Portfolio - Employer Competitive/assets/JS/main.js
@@ -1,1 +1,26 @@
 //Add on Click Events for each page
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $('.dropdown-trigger').dropdown();
+
+  
+  var instance = M.Dropdown.getInstance(elem);
+
+  /* jQuery Method Calls
+    You can still use the old jQuery plugin method calls.
+    But you won't be able to access instance properties.
+
+    $('.dropdown-trigger').dropdown('methodName');
+    $('.dropdown-trigger').dropdown('methodName', paramName);
+  */
+
+ instance.open();
+
+ instance.close();
+ instance.recalculateDimensions();
+ instance.destroy();
