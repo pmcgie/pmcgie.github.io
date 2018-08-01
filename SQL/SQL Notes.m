fce0f8941c828cff4be79ba84599965f2d64168f
pmcgie <pmcgie@gmail.com>
2018-07-31 21:16:04 -0500

    SQL and Server Examples Added

---
+++ SQL/SQL Notes.m
@@ -0,0 +1,235 @@
+```
+## MySQL and SQL Queries - Multiple Choice Readme
+
+
+### 1 
+
+1. SQL acronym:
+    * SQL stands for Structured Query Language
+    * From Week 12 Day 1 part time lesson plan section 2
+    * Tags: sql, vocab
+
+
+### 2 
+
+1. MySQL and MySQL Workbench / Sequel Pro relationship:
+    * MySQL runs on the server while MySQL Workbench and Sequel Pro are client implementations
+    * From Week 12 Day 1 part time lesson plan section 2 and 3
+    * Tags: mysql, sql-workbench, sequel-pro, vocab
+
+
+### 3 
+
+1. Creating databases:
+    * New databases can be made with CREATE DATABASE databasename;
+    * From Week 12 Day 1 part time lesson plan section 7
+    * Tags: sql, create-database, syntax
+
+
+### 4 
+
+1. SQL Data Storage:
+    * Databases store data in tables made up of columns
+    * From Week 12 Day 1 part time lesson plan section 8
+    * Tags: sql, tables, columns, vocab
+
+
+### 5 
+
+1. Selecting a database:
+    * USE is used to select a database to work on
+    * From Week 12 Day 1 part time lesson plan section 8
+    * Tags: sql, use, vocab
+
+
+### 6 
+
+1. Creating tables:
+    * New tables can be made with CREATE TABLE ();
+    * From Week 12 Day 1 part time lesson plan section 8
+    * Tags: sql, create-table, syntax
+
+
+### 7 
+
+1. Preventing empty fields:
+    * NOT NULL constraint prevents empty fields
+    * From Week 12 Day 1 part time lesson plan section 8
+    * Tags: sql, not-null, vocab
+
+
+### 8 
+
+1. Data type to hold variable length strings:
+    * VARCHAR data type used for specifying column holding variable length strings
+    * From Week 12 Day 1 part time lesson plan section 8
+    * Tags: sql, varchar, vocab
+
+
+### 9 
+
+1. Data type to hold whole numbers:
+    * INTEGER data type used for specifying column holding whole numbers
+    * From Week 12 Day 1 part time lesson plan section 8
+    * Tags: sql, integer, vocab
+
+
+### 10 
+
+1. Setting value when none specified:
+    * DEFAULT constraint sets a default column value
+    * From Week 12 Day 1 part time lesson plan section 10
+    * Tags: sql, default, vocab
+
+
+### 11 
+
+1. Adding data to tables:
+    * INSERT INTO statement used to add data to tables
+    * From Week 12 Day 1 part time lesson plan section 12
+    * Tags: sql, insert-into, vocab
+
+
+### 12 
+
+1. Adding data to tables 2:
+    * Specific data that is being added is preceeded by VALUES keyword
+    * From Week 12 Day 1 part time lesson plan section 12
+    * Tags: sql, values, vocab
+
+
+### 13 
+
+1. Duplicated code:
+    * Re-running the same insert statement will create a duplicate record
+    * From Week 12 Day 1 part time lesson plan section 12
+    * Tags: tech-knowledge
+
+
+### 14 
+
+1. Changing data:
+    * Data in a table can be changed with the UPDATE statement
+    * From Week 12 Day 1 part time lesson plan section 12
+    * Tags: sql, update, vocab
+
+
+### 15 
+
+1. Specifying data to change:
+    * SET keyword used to specify column and new value in an update
+    * From Week 12 Day 1 part time lesson plan section 12
+    * Tags: sql, set, vocab
+
+
+### 16 
+
+1. Use of WHERE:
+    * WHERE necessary when updating to prevent all records being modified
+    * From Week 12 Day 1 part time lesson plan section 12
+    * Tags: sql, where, tech-knowledge
+
+
+### 17 
+
+1. Removing data from tables:
+    * DELETE FROM statement used to remove data from tables
+    * From Week 12 Day 1 part time lesson plan section 14
+    * Tags: sql, delete-from, vocab
+
+
+### 18 
+
+1. Elements with shared values:
+    * GROUP BY statement used to group elements with shared values
+    * From Week 12 Day 3 part time lesson plan section 9
+    * Tags: sql, group-by, vocab
+    
+
+### 19 
+
+1. Selecting ranges of values:
+    * BETWEEN operator used to select range of values
+    * From Week 12 Day 3 part time lesson plan section 9
+    * Tags: sql, between, vocab
+
+
+### 20 
+
+1. Unique row identifier:
+    * Each row identifiable by unique primary key
+    * From Week 12 Day 1 part time lesson plan section 14
+    * Tags: primary-key, tech-knowledge
+
+
+### 21 
+
+1. Combining tables data:
+    * Joins can be used to combine data from different tables with a shared value
+    * From Week 12 Day 1 part time lesson plan section 17
+    * Tags: join, tech-knowledge
+
+
+### 22 
+
+1. NPM mysql's createConnection method:
+    * Requires user name, password, host, and database name
+    * From Week 12 Day 2 part time lesson plan section 2
+    * Tags: create-connection, mysql,  vocab
+
+
+### 23 
+
+1. Requirements to connect:
+    * Database must first be created before connection attempt
+    * From Week 12 Day 2 part time lesson plan section 3
+    * Tags: mysql, tech-knowledge
+
+
+### 24 
+
+1. More connections:
+    * SQL query is included in connection.query method
+    * From Week 12 Day 2 part time lesson plan section 4
+    * Tags: mysql, connection.query, syntax
+
+
+### 25 
+
+1. Purpose of connection.connect callback function:
+    * .query is place in .connect callback so query runs after connection is made
+    * From Week 12 Day 2 part time lesson plan section 4
+    * Tags: mysql, connection.connect, tech-knowledge
+
+
+### 26 
+
+1. Data returned from database:
+    * Response data to connection.query is an array of objects
+    * From Week 12 Day 2 part time lesson plan section 4
+    * Tags: mysql, tech-knowledge
+
+
+### 27 
+
+1. CRUD acronym:
+    * Create Read Update Delete
+    * From Week 12 Day 2 part time lesson plan section 7
+    * Tags: crud, vocab
+
+
+### 28 
+
+1. Database and Table creation code:
+    * schema.sql and seeds.sql for storing database and data startup code
+    * From Week 12 Day 3 part time lesson plan section 3
+    * Tags: mysql, schema.sql, seeds.sql, vocab
+
+
+### 29 
+
+1. Large database performance:
+    * Use indexes for speeding up query evaluation
+    * From Week 12 Day 3 part time lesson plan section 9
+    * Tags: index, tech-knowledge
+```
