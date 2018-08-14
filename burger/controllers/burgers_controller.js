a6ce44d3286e7da1f59b4916aab6fc9464fe5f5e
pmcgie <pmcgie@gmail.com>
2018-08-13 20:02:14 -0500

    a few more changes

--- burger/controllers/burgers_controller.js
+++ burger/controllers/burgers_controller.js
@@ -14,9 +14,8 @@ router.get("/", function(req,res) {
 
 // Creates a new burger object
 router.post("/api/burgers",function(req,res) {
-    burger.create(["burger_name"],[req.body.burger_name], 
-        function() {
-            res.redirect("/");
+    burger.create(["burger_name"],[req.body.burger_name], function() {
+        res.redirect("/");
     });
 
 });
@@ -34,6 +33,7 @@ router.put("/api/burgers/:id",function(req,res){
 
 // Removes Burger Entry
 router.delete("/api/burgers/:id", function(req,res){
+    
     var condition = "id = " + req.params.id;
 
     burger.delete(condition, function() {
