358e33f9bd114cf19c8112c902225667a04b4236
pmcgie <pmcgie@gmail.com>
2018-08-23 20:12:58 -0500

    More Coding Examples

---
+++ Misc Examples/Testing/01-Stu_Warmup/tax.js
@@ -0,0 +1,22 @@
+var calculateTax = function(price) {
+  // Fill in code here
+
+
+};
+
+module.exports = calculateTax;
+
+/*
+// Replace with code below to speed things up! 
+// Point of the exercise is to use mocha + chai in test/tax.test.js!
+
+var calculateTax = function(price) {
+  if (typeof price !== "number") {
+    throw new Error("Parameter 'price' is required and must be a number.");
+  }
+
+  return Number((price * 1.08).toFixed(2));
+};
+
+module.exports = calculateTax;
+*/
