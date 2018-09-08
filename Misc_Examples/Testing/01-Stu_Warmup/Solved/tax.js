a4e5d256f0e592861235401e8b72cf2966e98136
pmcgie <pmcgie@gmail.com>
2018-09-08 10:57:56 -0500

    updates

--- Misc_Examples/Testing/01-Stu_Warmup/Solved/tax.js
+++
@@ -1,9 +0,0 @@
-var calculateTax = function(price) {
-  if (typeof price !== "number") {
-    throw new Error("Parameter `price` is required and must be a number.");
-  }
-
-  return Number((price * 1.08).toFixed(2));
-};
-
-module.exports = calculateTax;
